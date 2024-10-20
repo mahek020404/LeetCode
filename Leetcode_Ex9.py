688bc753cdbda9eb2f0e80e6c2a049461e17ad70
mahek020404 <mahekpatel020404@gmail.com>
2024-10-21 00:28:42 +0530

    Add files via upload

---
+++ Leetcode_Ex9.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        seen = set()  
+        for num in nums:
+            if num in seen:
+                return True  
+            seen.add(num) 
+        return False  
+s = Solution()
+# Example 1:
+print(s.containsDuplicate([1, 2, 3, 1]))  
+# Example 2:
+print(s.containsDuplicate([1, 2, 3, 4]))  
+# Example 3:
+print(s.containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))  
