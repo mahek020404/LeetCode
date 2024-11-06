e09537caeaa063eef17463774c79b57efa28635e
mahek020404 <mahekpatel020404@gmail.com>
2024-11-06 22:42:03 +0530

    Create Leetcode_Ex11.java

---
+++ Leetcode_Ex11.java
@@ -0,0 +1,15 @@
+https://leetcode.com/problems/reverse-bits/
+
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) 
+        {
+            result <<= 1;
+            result |= (n & 1);
+            n >>= 1;
+        } 
+        return result;
+    }
+}
