fd7b4f9ea6f6904d6bf90ba5975c1ee1148d68c1
mahek020404 <mahekpatel020404@gmail.com>
2024-11-09 11:24:57 +0530

    Add files via upload

---
+++ Solution10.java
@@ -0,0 +1,22 @@
+//BINARY TREE INORDER TRAVERSAL
+
+class Solution10 {
+    public List<Integer> inorderTraversal(TreeNode root) 
+    {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;        
+        while (current != null || !stack.isEmpty()) 
+        {
+            while (current != null) 
+            {
+                stack.push(current);
+                current = current.left;
+            }
+            current = stack.pop();
+            result.add(current.val);
+            current = current.right;
+        }
+        return result;
+    }
+}
